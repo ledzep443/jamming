91180e3a8c06e0b3edb40bc25a4680b4e2849f0f
ledzep443 <jimmypurnell27@gmail.com>
2021-03-27 02:22:50 -0400

    Fixed error

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,22 +3,24 @@ import Playlist from '../Playlist/Playlist.js';
 import Searchbar from '../SearchBar/SearchBar.js';
 import SearchResults from '../SearchResults/SearchResults.js';
 import { render } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
+class App extends React.Component {
     render() {
         return ( 
         <div>
-        <h1 > Ja < span className = "highlight" > mmm </span>ing</h1>
-        <div class = "App" >
-        <Searchbar />
-        <div className = "App-playlist" >
-        <SearchResults />
-        <Playlist />
-        </div>   
-        </div >  
-    </div >
-    );
-}
+            <h1 > Ja < span className = "highlight" > mmm </span>ing</h1>
+            <div class = "App" >
+                <Searchbar />
+                <div className = "App-playlist" >
+                    <SearchResults />
+                    <Playlist />
+                </div>   
+            </div >  
+        </div >
+        );
+    }
 }
 
 export default App;
